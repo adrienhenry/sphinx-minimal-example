6ac5b7681fcfc4aea7bbbf7edcd987f722e8bef7
Adrien Henry <adrien.ym.henry@gmail.com>
2016-10-26 14:05:17 -0400

    First Commit

---
+++ MyClass/function.py
@@ -0,0 +1,10 @@
+def hello_world(name):
+    """ Function that says hello to someone
+
+    Args:
+        name(string): Name of the person to greet
+    Returns:
+        None	
+    """
+    print("Hello "+name)
+    return None
---
+++ main.py
@@ -0,0 +1,3 @@
+import MyClass
+
+MyClass.hello_world("Spam")
